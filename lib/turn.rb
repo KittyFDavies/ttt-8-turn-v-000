69f03cc6da9e911a194e50ecebdc3f5bca84d725
Kitty Davies <kittyfdavies@gmail.com>
2018-02-28 00:26:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,7 +42,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, "X")
-  else while false
-    puts "Please enter 1-9:"
+  #else while false
+  #  puts "Please enter 1-9:"
   end
 end
